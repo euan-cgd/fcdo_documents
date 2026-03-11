ba54810eb19f9535d4b5a97f74c21c12713a2f77
euan-cgd <eritchie@cgdev.org>
2026-03-11 17:54:20 +0000

    Create get_doc_links.py
    get the links to fcdo documents from iati pages

---
+++ get_doc_links.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import requests
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+import time
+from lxml import etree
+import numpy as np
+import pickle
+
+url = "https://dashboard.iatistandard.org/publishers/fcdo/"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+datatable = soup.find_all('table',class_="exploring-data")
+fcdolinks = datatable[0].find_all("a",string="Source Url")
+fcdolinks = [x.get("href") for x in fcdolinks]
+
+fcdolinks = [link for link in fcdolinks if 'xml' in link]
+fcdolinks = [link for link in fcdolinks if 'organisation' not in link]
+
+##-----------------------------------------------------------------------
+## GET LINKS TO PROJECT DOCUMENTS
+
+def read_xml(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Check if the request was successful
+        return etree.fromstring(response.content)
+    except requests.exceptions.RequestException as e:
+        return "fail"
+
+collectdocs = {}
+
+for lnk in fcdolinks:  
+    proj = re.sub(r".*/", "", lnk)
+    proj = re.sub(r"\.xml$", "", proj)
+    
+    link = read_xml(lnk)
+    
+    if link == "fail":
+        continue
+
+    docnodes = link.xpath("//document-link/title/narrative")
+    if len(docnodes) == 0:
+        continue
+    else:
+        docnarr = [node.text for node in docnodes]
+        doclink = [node.xpath('../..')[0].attrib.get('url') for node in docnodes]
+        docid   = [node.xpath('../../../*')[0].text for node in docnodes] 
+        docttl  = [node.xpath('../../../title/narrative')[0].text for node in docnodes]# Adjust 'child' to the correct tag if necessary
+        docs    = pd.DataFrame({
+            "narrative": docnarr,
+            "link": doclink,
+            "parent": docid,
+            "proj": proj,
+            "title":docttl
+        })
+    collectdocs[proj] = docs
+
+collectdocs = pd.concat(collectdocs, ignore_index=True)    
+
+collectdocs = collectdocs.drop_duplicates()  # Drop duplicate rows
+
+# Convert the 'narrative' column to lowercase
+collectdocs.loc[:,'narrative'] = collectdocs['narrative'].str.lower()
+
+# Create new columns based on whether certain patterns are found in the 'narrative' column
+collectdocs.loc[:,'LF'] = collectdocs['narrative'].str.contains(r"logical ?framework").astype(int)
+collectdocs.loc[:,'PCR'] = collectdocs['narrative'].str.contains(r"project ?completion ?review").astype(int)
+collectdocs.loc[:,'AR'] = collectdocs['narrative'].str.contains(r"annual ?review").astype(int)
+collectdocs.loc[:,'BC'] = collectdocs['narrative'].str.contains(r"business ?case").astype(int)
+collectdocs.loc[:,'IS'] = collectdocs['narrative'].str.contains(r"intervention ?summary").astype(int)
+
+# Just ex-DFID bits
+dfid = collectdocs[(~collectdocs["parent"].str.contains('GOV-3')) & ((collectdocs.AR==1) | (collectdocs.BC==1) | (collectdocs.PCR==1))]
+dfid.loc[:,'ADD'] = dfid.loc[:,'narrative'].str.contains(r'addendum', case=False).astype(int)
